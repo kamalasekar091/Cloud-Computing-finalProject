b71f1751d6bfc6ca826548a22e52cd758639e739
kamala Sekar Rose <kamalasekar091@gmail.com>
2016-10-25 16:50:19 +0000

    changing installapp.sh for week 08-attempt 01

--- installapp.sh
+++ installapp.sh
@@ -5,10 +5,12 @@ sudo apt-get update -y
 sudo apt-get install -y apache2
 sudo systemctl enable apache2
 sudo systemctl start apache2
+git clone git@github.com:illinoistech-itm/krose1.git
 cd /var/www
 sudo apt-get install -y git
+#git clone git@github.com:illinoistech-itm/krose1
 git clone https://github.com/kamalasekar091/boostrap-website
 cd html
 sudo rm -rf *
 cd ..
-sudo cp -a boostrap-website/. html/
+sudo cp -a krose1/. html/
