c159bb6eae981c738e5b7e686fae23dd796f5eb0
kamala Sekar Rose <kamalasekar091@gmail.com>
2016-10-27 16:59:04 +0000

    Adding the files to private repo

--- installapp.sh
+++ installapp.sh
@@ -5,7 +5,13 @@ sudo apt-get update -y
 sudo apt-get install -y apache2
 sudo systemctl enable apache2
 sudo systemctl start apache2
+ssh-agent -s
+eval "$(ssh-agent -s)"
+ssh-add /home/ubuntu/.ssh/id_rsa
+ssh-keyscan -t rsa github.com >> /home/ubuntu/.ssh/known_hosts
 git clone git@github.com:illinoistech-itm/krose1.git
+
+
 cd /var/www
 sudo apt-get install -y git
 #git clone git@github.com:illinoistech-itm/krose1
